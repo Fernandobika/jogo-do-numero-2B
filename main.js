32f8f4f10c1e97d4b46f5990c81d400eab8915c0
Fernandobika <alves.rodrigues.fernando@escola.pr.gov.br>
2025-08-07 07:43:39 -0300

    .

---
+++ main.js
@@ -0,0 +1,21 @@
+
+const palpites = document.querySelector('.palpites');
+const ultimoResultado= document.querySelector('.ultimoResultado');
+const baixoOuAlto= document.querySelector('.baixoOuAlto');
+const envioPalpite= document.querySelector('.envioPalpite');
+const campoPalpite= querySelector('.campoPalpite');
+
+
+
+function verificarPalpite(){
+    const palpiteUsuario= Number(campoPalpite.value);
+    if(contagemPalpites === 1) {
+        palpiteUsuario.textContent ="Palpites anteriores:"
+    }
+
+    palpites.textContent +=palpiteUsuario +" ";
+
+    if(palpiteUsuario === numeroAleatorio){
+        ultimoResultado.textContent ="Parabens! Voce Acertou";
+    }
+}
